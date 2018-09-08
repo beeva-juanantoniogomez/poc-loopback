df1888f9323c1e13f53f4f6e1edaf8afd3b242f6
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-09-08 20:01:40 +0200

    Add relations between artists and movies

--- server/boot/seeds/artists.js
+++ server/boot/seeds/artists.js
@@ -6,13 +6,11 @@ const records = function() {
   return [{
     fullname: 'Footano',
     birthdate: moment('15/02/1960', 'DD/MM/YYYY').toDate(),
-    country: 'eeuu',
     photo: 1990,
     gender: 'male',
   }, {
     fullname: 'Bariano',
     birthdate: moment('15/02/1960', 'DD/MM/YYYY').toDate(),
-    country: 'eeuu',
     photo: 'https://my-image.com/02',
     gender: 'female',
   }];
--- server/boot/seeds/movies.js
+++ server/boot/seeds/movies.js
@@ -6,19 +6,15 @@ const records = function() {
     duration: 120,
     poster: 'https://my-image.com/01',
     year: 1990,
-    country: 'eeuu',
     sinopsis: 'Lorem ipsum',
     rating_filmaffinity: 4,
-    publisher: 'Garbage de potanzos',
   }, {
     title: 'Foo Bar 2',
     duration: 130,
     poster: 'https://my-image.com/02',
     year: 1995,
-    country: 'eeuu',
     sinopsis: 'Ipsum dolor',
     rating_filmaffinity: 3,
-    publisher: 'Garbage de potanzos',
   }];
 };
 
