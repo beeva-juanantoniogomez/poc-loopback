7a968d4fb17cb5af6af751c792a317a99127b021
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2018-09-08 20:00:35 +0200

    Add User seeds

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -6,6 +6,7 @@ const {log} = console;
 const SEEDS_MOVIES = require('./seeds/movies')();
 const SEEDS_ARTISTS = require('./seeds/artists')();
 const SEEDS_COUNTRIES = require('./seeds/countries')();
+const SEEDS_USERS = require('./seeds/users')();
 
 module.exports = function(app) {
   const models = app.models;
@@ -14,12 +15,14 @@ module.exports = function(app) {
     movies: async.apply(createMovies),
     artists: async.apply(createArtists),
     countries: async.apply(createCountries),
+    users: async.apply(createUsers),
   }, function(err, results) {
     if (err) throw err;
 
     log(`Created ${results.movies.length} movies`);
     log(`Created ${results.artists.length} artists`);
     log(`Created ${results.countries.length} countries`);
+    log(`Created ${results.users.length} users`);
   });
 
   function createMovies(cb) {
@@ -32,5 +35,9 @@ module.exports = function(app) {
 
   function createCountries(cb) {
     models.Country.create(SEEDS_COUNTRIES, cb);
+  }
+
+  function createUsers(cb) {
+    models.User.create(SEEDS_USERS, cb);
   }
 };
