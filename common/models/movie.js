8f3b69c513e54a1d70b98d1d8242633ce6918ea2
Juan Antonio Gómez Benito <juanantonio.gomez@beeva.com>
2018-12-19 20:40:57 +0100

    Improve Movie model

--- common/models/movie.js
+++ common/models/movie.js
@@ -2,7 +2,7 @@
 
 module.exports = function(Movie) {
   Movie.beforeRemote('create', function(context, user, next) {
-    context.args.data.date = Date.now();
+    context.args.data.created = Date.now();
     context.args.data.publisherId = context.req.accessToken.userId;
     next();
   });
